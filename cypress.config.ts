e14e57a44c93f3a91b5c37a42a0a2af11d174fdf
Cristian De Gracia Nuero <cdegracia@tribalyte.com>
2023-06-05 20:52:18 +0200

    Add playwright

--- cypress.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'cypress'
-
-export default defineConfig({
-  e2e: {
-    setupNodeEvents(_on, config) {
-      config.video = false
-    },
-  },
-})
--- cypress/e2e/test.cy.ts
+++
@@ -1,4 +0,0 @@
-it('App loads correctly', () => {
-  cy.visit('http://localhost:5173/')
-  cy.get('h1').contains('Vite + React')
-})
