bb7028b72896342dc0e000c2d0dad640c217eea4
cristian <cdegracia@tribalyte.com>
2023-09-18 09:36:43 +0200

    Add unit tests for users

--- api/src/resources/users/utils/is-valid-dto.ts
+++ api/src/resources/users/utils/is-valid-dto.ts
@@ -4,13 +4,17 @@ import { InvalidDtoError } from '../../../errors/types';
 
 export const isValidCreateUserDto = (body: unknown): Effect.Effect<never, InvalidDtoError, CreateUserDto> => {
   const { email, username, password, repeatedPassword } = body as CreateUserDto;
-  return !!email && !!username && !!password && !!repeatedPassword
+
+  const requiredFieldsPresent = !!email && !!username && !!password && !!repeatedPassword;
+  const passwordsMatch = password === repeatedPassword;
+
+  return requiredFieldsPresent && passwordsMatch
     ? Effect.succeed({ email, username, password, repeatedPassword })
     : Effect.fail(new InvalidDtoError({}));
 };
 
 // TODO implement password change
 export const isValidUpdateUserDto = (body: unknown): Effect.Effect<never, InvalidDtoError, UpdateUserDto> => {
-  const { username } = body as UpdateUserDto;
-  return !!username ? Effect.succeed({ username }) : Effect.fail(new InvalidDtoError({}));
+  const { username, email } = body as UpdateUserDto;
+  return !!username || !!email ? Effect.succeed({ username, email }) : Effect.fail(new InvalidDtoError({}));
 };
--- api/test/unit/resources/users/utils/is-valid-dto.test.ts
+++ api/test/unit/resources/users/utils/is-valid-dto.test.ts
@@ -1,11 +1,97 @@
-describe('isValidCreateUserDto()', () => {
-  it.todo('valid dto');
+import { Effect } from 'effect';
+import { isValidCreateUserDto, isValidUpdateUserDto } from '../../../../../src/resources/users/utils';
 
-  it.todo('invalid dto');
+describe('isValidCreateUserDto()', () => {
+  it('valid dto', () => {
+    const validDto = {
+      email: 'user@example.com',
+      username: 'username',
+      password: 'password',
+      repeatedPassword: 'password',
+    };
+
+    const dto = Effect.runSync(isValidCreateUserDto(validDto));
+
+    expect(dto).toEqual(validDto);
+  });
+
+  it('missing email', () => {
+    const invalidDto = {
+      username: 'username',
+      password: 'password',
+      repeatedPassword: 'password',
+    };
+
+    const effect = isValidCreateUserDto(invalidDto);
+
+    expect(() => Effect.runSync(effect)).toThrow('Invalid dto');
+  });
+
+  it('missing username', () => {
+    const invalidDto = {
+      email: 'user@example.com',
+      password: 'password',
+      repeatedPassword: 'password',
+    };
+
+    const effect = isValidCreateUserDto(invalidDto);
+
+    expect(() => Effect.runSync(effect)).toThrow('Invalid dto');
+  });
+
+  it('missing password', () => {
+    const invalidDto = {
+      email: 'user@example.com',
+      username: 'username',
+      repeatedPassword: 'password',
+    };
+
+    const effect = isValidCreateUserDto(invalidDto);
+
+    expect(() => Effect.runSync(effect)).toThrow('Invalid dto');
+  });
+
+  it('missmatching passwords', () => {
+    const invalidDto = {
+      email: 'user@example.com',
+      username: 'username',
+      password: 'password',
+      repeatedPassword: 'differentpassword',
+    };
+
+    const effect = isValidCreateUserDto(invalidDto);
+
+    expect(() => Effect.runSync(effect)).toThrow('Invalid dto');
+  });
 });
 
 describe('isValidUpdateUserDto()', () => {
-  it.todo('valid dto');
+  it('valid dto', () => {
+    const validDto = {
+      email: 'user@example.com',
+      username: 'new-username',
+    };
 
-  it.todo('invalid dto');
+    const dto = Effect.runSync(isValidUpdateUserDto(validDto));
+
+    expect(dto).toEqual(validDto);
+  });
+
+  it('partial dto', () => {
+    const validDto = {
+      username: 'new-username',
+    };
+
+    const dto = Effect.runSync(isValidUpdateUserDto(validDto));
+
+    expect(dto).toEqual(validDto);
+  });
+
+  it('invalid dto', () => {
+    const invalidDto = {};
+
+    const effect = isValidUpdateUserDto(invalidDto);
+
+    expect(() => Effect.runSync(effect)).toThrow('Invalid dto');
+  });
 });
