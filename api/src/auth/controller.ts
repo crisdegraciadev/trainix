ac40f8bd4ddd7388e45b2e73c09c15b6ccc4df22
cristian <cdegracia@tribalyte.com>
2023-10-18 17:09:36 +0200

    (feat #5): Add debug trace

--- api/src/auth/controller.ts
+++ api/src/auth/controller.ts
@@ -10,7 +10,7 @@ import { handleFailureCauses } from '../errors/handlers';
 export const handleLogin = async (req: Request, res: Response): Promise<void> => {
   const { body } = req;
 
-  console.log({ body });
+  console.log({ body, Auth });
 
   const accessTokenResult = await pipe(
     Effect.all([isValidLoginDto(body)]),
